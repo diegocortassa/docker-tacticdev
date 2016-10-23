4f09bb80411a8b9f386cb6ef69d6eab517ba546d
Diego Cortassa <diego@cortassa.net>
2016-10-23 12:42:19 +0200

    Renamed docker.io to docker for ubuntu 16.04

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 TIMESTAMP=`date +%Y-%m-%d`
-alias docker='sudo docker.io'
+alias docker='sudo docker'
 sed -i "s/ENV REFRESHED_AT .*$/ENV REFRESHED_AT $TIMESTAMP/" Dockerfile
 docker build -t diegocortassa/tacticdev .
 docker push diegocortassa/tacticdev
