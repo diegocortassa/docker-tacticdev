a601d10664b3997c566e0cadffbf5a9a4aafc7a5
Diego Cortassa <diego@cortassa.net>
2022-04-02 12:03:19 +0200

    update to 4.9.0.a01

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 TIMESTAMP=`date +%Y-%m-%d`
 sed -i "s/ENV REFRESHED_AT .*$/ENV REFRESHED_AT $TIMESTAMP/" Dockerfile
 sudo docker build -t diegocortassa/tacticdev .
-sudo docker build -t diegocortassa/tacticdev:4.8 .
-sudo docker build -t diegocortassa/tacticdev:4.8.0 .
+sudo docker build -t diegocortassa/tacticdev:4.9 .
+sudo docker build -t diegocortassa/tacticdev:4.9.0.a01 .
 sudo docker push diegocortassa/tacticdev
-sudo docker push diegocortassa/tacticdev:4.8
-sudo docker push diegocortassa/tacticdev:4.8.0
+sudo docker push diegocortassa/tacticdev:4.9
+sudo docker push diegocortassa/tacticdev:4.9.0.a01
